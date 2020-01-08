4eb1d6e9583a6bdb3d22c6cb9ea2dacf9697aaa7
guzele <Gisele.Garifullina@gmail.com>
2020-01-09 01:10:05 +0500

    First simple version

---
+++ bot.js
@@ -0,0 +1,19 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+require('dotenv').config();
+
+const token = process.env.TELEGRAM_TOKEN;
+let bot;
+
+if (process.env.NODE_ENV === 'production') {
+    bot = new TelegramBot(token);
+    bot.setWebHook(process.env.HEROKU_URL + bot.token);
+} else {
+    bot = new TelegramBot(token, { polling: true });
+}
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+    const chatId = msg.chat.id;
+    const word = match[1];
+    bot.sendMessage(chatId, word);
+});
