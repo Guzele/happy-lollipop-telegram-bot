3952ed5e41765b9168dbed8d5ab9acbc114a1d35
guzele <Gisele.Garifullina@gmail.com>
2020-01-09 18:19:39 +0500

    Updated server files to make it work?

--- bot.js
+++ bot.js
@@ -1,19 +1,23 @@
-const TelegramBot = require('node-telegram-bot-api');
-
-require('dotenv').config();
-
 const token = process.env.TELEGRAM_TOKEN;
+
+const Bot = require('node-telegram-bot-api');
 let bot;
 
-if (process.env.NODE_ENV === 'production') {
-    bot = new TelegramBot(token);
-    bot.setWebHook(process.env.HEROKU_URL + bot.token);
-} else {
-    bot = new TelegramBot(token, { polling: true });
+if(process.env.NODE_ENV === 'production') {
+  bot = new Bot(token);
+  bot.setWebHook(process.env.HEROKU_URL + bot.token);
+}
+else {
+  bot = new Bot(token, { polling: true });
 }
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
-    const chatId = msg.chat.id;
-    const word = match[1];
-    bot.sendMessage(chatId, word);
+console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
+
+bot.on('message', (msg) => {
+  const name = msg.from.first_name;
+  bot.sendMessage(msg.chat.id, 'Hello, ' + name + '!').then(() => {
+    // reply sent!
+  });
 });
+
+module.exports = bot;
